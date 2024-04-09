fec36aaa52844e81708bd5e1f39190da382f29c0
Adrian <adferrer0611@gmail.com>
2024-04-08 21:08:41 -0400

    MaterialUI and TailwindCSS integration done

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { taskStore } from "./redux/stores/store.ts";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Provider store={taskStore}>
-            <App />
-        </Provider>
+        <StyledEngineProvider injectFirst>
+            <Provider store={taskStore}>
+                <CssBaseline />
+                <App />
+            </Provider>
+        </StyledEngineProvider>
     </React.StrictMode>
 );
